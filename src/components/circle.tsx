cd5a16581e6e3fd66c0d7209e01e0b39b658cc5a
Petros <93052465+pkitazos@users.noreply.github.com>
2026-02-22 21:50:32 +0200

    hotfix: added links to project list

--- src/components/circle.tsx
+++ src/components/circle.tsx
@@ -14,6 +14,6 @@ export default function Circle({ className }: { className: ClassValue }) {
         duration: 0.4,
         scale: { type: "spring", visualDuration: 0.4, bounce: 0.5 },
       }}
-    ></motion.div>
+    />
   );
 }
